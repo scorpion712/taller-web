674be8b6180197e487722767071c1ae4d325bb5c
scorpion712 <lauutii.127@gmail.com>
2023-03-11 19:19:58 -0300

    Refactor product routes

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -4,11 +4,10 @@ const ctrlProducts = require('../controller/ctrlProducts');
 const router = express.Router();
 
 // Products routes 
-router.get('/products', ctrlProducts.fetchProducts); // route to get all products or filter by query
-router.get('/products/:product_id', ctrlProducts.getProductById); // route to get product by id
-router.post('/products', ctrlProducts.createProduct); // route to create a new product
-router.delete('/products/:product_id', ctrlProducts.deleteProduct); // route to delete product by given id
-router.put('/products/:product_id', ctrlProducts.updateProduct); // route to update product by given id
+router.get('/products', ctrlProducts.fetchProducts); // route to get all products
+router.get('/products/:product_id', ctrlProducts.getProductById); // route to get product details
+router.post('/products/:product_id', ctrlProducts.updateProduct); // route to update product stock by given id
 
+router.get('/seed/products', ctrlProducts.mockProducts); // route to mock products
 
 module.exports = router;
