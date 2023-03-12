1930d786a192470603bd166672c5b0d7fb4c2492
scorpion712 <lauutii.127@gmail.com>
2023-03-12 17:35:17 -0300

    Product detail to cart item adapter

--- frontend/src/main/adapters/cartItem.adapter.ts
+++ frontend/src/main/adapters/cartItem.adapter.ts
@@ -1,5 +1,5 @@
 import { CartItem } from "../../cart/models/cart.model";
-import { Product} from "../../models/Product.model"; 
+import { Product, ProductDetail } from "../../models/Product.model";
 
 export const adaptCartItem = (product: Product) => {
     const formattedItem: CartItem = {
@@ -11,5 +11,19 @@ export const adaptCartItem = (product: Product) => {
         thumbnail: product.thumbnail,
     };
     return formattedItem;
-  };
-  
+};
+
+export const adaptCartItemProductDetail = (product?: ProductDetail) => {
+    if (product) {
+        const formattedItem: CartItem = {
+            id: product._id,
+            title: product.title,
+            price: product.price,
+            discountPercentage: product.discountPercentage,
+            units: 1,
+            thumbnail: product.thumbnail,
+        };
+        return formattedItem;
+    }
+    return {} as CartItem;
+};
