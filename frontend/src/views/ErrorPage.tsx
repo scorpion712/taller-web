37b7cffca52ad9168b1df823de088a61dfc95503
scorpion712 <lauutii.127@gmail.com>
2023-03-12 22:06:54 -0300

    Create ErrorPage

--- frontend/src/views/ErrorPage.tsx
+++ frontend/src/views/ErrorPage.tsx
@@ -1,15 +1,14 @@
+import { Box, Typography } from "@mui/material";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error: any = useRouteError(); 
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Box id="error-page" style={{display:'flex', justifyContent: 'center', alignContent:'center', flexDirection: 'column', alignItems:'center', marginTop:'10rem'}}>
+      <Typography variant="h1" gutterBottom>Oops!</Typography>
+      <Typography variant="h5" >Sorry, an unexpected error has occurred.</Typography>
+      <Typography variant="h6" fontStyle={'italic'} gutterBottom color='text.secondary'>{error.statusText || error.message}</Typography>
+    </Box>
   );
 }
