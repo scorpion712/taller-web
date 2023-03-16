c4ae8d5f362fd19fbe7dd0bef427a25d929557e4
scorpion712 <lauutii.127@gmail.com>
2023-03-15 23:22:58 -0300

    Add Auth Context to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,48 @@
 import { createTheme, ThemeProvider } from "@mui/material";
-import "./App.css"; 
+import "./App.css";
 import CartState from "./components/cart/context/CartState";
-import ProductState from "./context/product/ProductState"; 
+import ProductState from "./context/product/ProductState";
 import { Router } from "./router/Router";
- 
-declare module '@mui/material/styles' {
+import AuthState from "./views/login/context/AuthState";
+
+declare module "@mui/material/styles" {
   interface Theme {
     status: {
-      danger: React.CSSProperties['color'];
+      danger: React.CSSProperties["color"];
     };
-  } 
- 
+  }
+
   interface ThemeOptions {
     status: {
-      danger: React.CSSProperties['color'];
+      danger: React.CSSProperties["color"];
     };
   }
 }
 
-
 const theme = createTheme({
   status: {
-    danger: '#e53e3e',
+    danger: "#e53e3e",
   },
   palette: {
     primary: {
-      main: '#003057', 
+      main: "#003057",
     },
     secondary: {
-      main: '#75787B', 
+      main: "#75787B",
     },
   },
 });
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CartState>
-        <ProductState>
-          <Router/>
-        </ProductState>
-      </CartState>
+      <AuthState>
+        <CartState>
+          <ProductState>
+            <Router />
+          </ProductState>
+        </CartState>
+      </AuthState>
     </ThemeProvider>
   );
 }
